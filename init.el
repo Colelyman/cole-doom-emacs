e388a1fb9b89c1126f6c971b06044c650eace102
Cole Lyman <cole@colelyman.com>
2018-07-08 21:22:30 -0600

    Added eshell support

--- init.el
+++ init.el
@@ -52,7 +52,7 @@
        dired             ; making dired pretty [functional]
        ediff             ; comparing files in Emacs
        electric          ; smarter, keyword-based electric-indent
-      ;eshell            ; a consistent, cross-platform shell (WIP)
+       eshell            ; a consistent, cross-platform shell (WIP)
        imenu             ; an imenu sidebar and searchable code index
        term              ; terminals in Emacs
        vc                ; version-control and Emacs, sitting in a tree
