c63ea4b1e0432eb9f4cfcaef4d645c86f7d13070
Cole Lyman <cole@colelyman.com>
2018-07-31 19:02:59 -0600

    Corrected much of the org-ref variables, added new local org mappings

--- org.el
+++ org.el
@@ -15,6 +15,7 @@
            "* TODO %? %^g\nDEADLINE: %^t")
           ("h" "Hugo post" entry (file+olp "~/code/colelyman-hugo/site/content-org/posts.org" "Blog Ideas")
            (function org-hugo-new-subtree-post-capture-template))))
+  (add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
   (advice-add 'org-deadline :after 'org-save-all-org-buffers)
   (advice-add 'org-refile :after 'org-save-all-org-buffers))
 
@@ -24,16 +25,14 @@
   :init
   (setq org-ref-completion-library 'org-ref-ivy-cite)
   :config
-  (setq org-ref-directory (concat (file-name-as-directory org-directory) "papers")
+  (setq org-ref-note-title-format "* TODO %y - %t\n :PROPERTIES:\n  :Custom_ID: %k\n  :INTERLEAVE_PDF: ./lib/%k.pdf\n  :AUTHOR: %9a\n  :JOURNAL: %j\n  :YEAR: %y\n  :VOLUME: %v\n  :PAGES: %p\n  :DOI: %D\n  :URL: %U\n :END:\n\n"
+        org-ref-directory (concat (file-name-as-directory org-directory) "papers/")
         org-ref-notes-directory org-ref-directory
-        org-ref-bibliography-notes (concat org-ref-directory
-                                           (file-name-as-directory "index.org"))
-        org-ref-default-bibliography '((concat org-ref-directory
-                                               (file-name-as-directory "index.bib")))
+        org-ref-bibliography-notes (concat org-ref-directory "index.org")
+        org-ref-default-bibliography '((concat org-ref-directory "index.bib"))
         org-ref-pdf-directory (concat org-ref-directory
                                       (file-name-as-directory "lib"))
-        bibtex-completion-bibliography (concat org-ref-directory
-                                               (file-name-as-directory "index.bib"))
+        bibtex-completion-bibliography (concat org-ref-directory "index.bib")
         bibtex-completion-library-path org-ref-pdf-directory
         bibtex-completion-notes-path org-ref-bibliography-notes))
 
@@ -44,3 +43,12 @@
   :config
   (add-hook! org-capture-before-finalize #'+cole/org-capture--remove-auto-org-to-hugo-export-maybe)
   (add-hook! org-capture-after-finalize #'+cole/org-capture--add-auto-org-to-hugo-export-maybe))
+
+(def-package! interleave
+  :defer t)
+
+(map! (:after org
+        (:map org-mode-map
+          :localleader
+          :desc "Edit SRC block" :nvm "'" #'org-edit-src-code
+          :desc "Toggle interleave mode" :nvm "i" #'interleave-mode)))
